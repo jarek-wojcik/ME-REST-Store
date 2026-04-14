7cbf9ef3b1989032316623464f3ff27d4a0b276a
jarek-wojcik <jarek.wojcik@live.com>
2026-04-14 22:20:20 +0100

    Fixing the ability to filter to specific enemy types.

--- SFS_Core_ASI/SFSWebserver/controllers/mission_params_controller.go
+++ SFS_Core_ASI/SFSWebserver/controllers/mission_params_controller.go
@@ -65,7 +65,7 @@ func (c *MissionParamsController) save(ms model.MissionSettings) error {
 
 func (c *MissionParamsController) render(w http.ResponseWriter, ms model.MissionSettings) {
 	vm := missionParamsViewData{
-		MissionSettings:  ms,
+		MissionSettings:   ms,
 		EnabledEnemiesMap: make(map[string]bool, len(ms.EnabledEnemies)),
 	}
 	for _, e := range ms.EnabledEnemies {
